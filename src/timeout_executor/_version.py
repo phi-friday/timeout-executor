c885d423ac6e617a93c415ab36b429be0e5c3730
phi <phi.friday@gmail.com>
2023-06-25 08:11:36 +0900

    fix: action

--- src/timeout_executor/_version.py
+++ src/timeout_executor/_version.py
@@ -1,20 +1,3 @@
 from __future__ import annotations
 
-import sys
-from pathlib import Path
-
-if sys.version_info < (3, 11):
-    import tomli as tomllib  # type: ignore
-else:
-    import tomllib  # type: ignore
-
-__all__ = ["__version__"]
-
-pyproject_path = Path(__file__).parent / "pyproject.toml"
-try:
-    with pyproject_path.open("rb") as f:
-        pyproject = tomllib.load(f)
-except FileNotFoundError:
-    with pyproject_path.parent.parent.with_name(pyproject_path.name).open("rb") as f:
-        pyproject = tomllib.load(f)
-__version__: str = pyproject["tool"]["poetry"]["version"]
+__version__: str = "0.0.0"
