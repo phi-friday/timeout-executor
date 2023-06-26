870173d18893167fa25aa42cb7af0fee99a25490
phi <phi.friday@gmail.com>
2023-06-26 20:12:16 +0900

    fix: check loky pickler before set

--- src/timeout_executor/pickler/_loky/patch.py
+++ src/timeout_executor/pickler/_loky/patch.py
@@ -16,18 +16,28 @@ def monkey_patch(name: str, pickler: Pickler) -> None:  # noqa: ARG001
         logger.warning("loky uses cloudpickle as the default")
         name = "cloudpickle"
     try:
-        from loky.backend.reduction import set_loky_pickler  # type: ignore
+        from loky.backend.reduction import (  # type: ignore
+            get_loky_pickler_name,  # type: ignore
+            set_loky_pickler,  # type: ignore
+        )
     except (ImportError, ModuleNotFoundError) as exc:
         raise ImportError("install extra first: loky") from exc
 
+    if get_loky_pickler_name() == name:
+        return
     set_loky_pickler(name)
 
 
 def monkey_unpatch() -> None:
     """unpatch loky"""
     try:
-        from loky.backend.reduction import set_loky_pickler  # type: ignore
+        from loky.backend.reduction import (  # type: ignore
+            get_loky_pickler_name,  # type: ignore
+            set_loky_pickler,  # type: ignore
+        )
     except (ImportError, ModuleNotFoundError) as exc:
         raise ImportError("install extra first: loky") from exc
 
+    if get_loky_pickler_name == "cloudpickle":
+        return
     set_loky_pickler()
