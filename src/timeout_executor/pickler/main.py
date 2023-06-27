16d61a42ef0ce8ffb180464fd5fe8975b697d5df
phi <phi.friday@gmail.com>
2023-06-27 22:54:07 +0900

    fix: add debug logs

--- src/timeout_executor/pickler/main.py
+++ src/timeout_executor/pickler/main.py
@@ -17,13 +17,14 @@ PicklerType = Literal["pickle", "dill", "cloudpickle"]
 def monkey_patch(backend: BackendType, pickler: PicklerType | None) -> None:
     """monkey patch or unpatch"""
     backend_module = _import_backend(backend)
-    pickler = _validate_pickler(backend_module, pickler)
+    pickler = _validate_pickler(backend, backend_module, pickler)
     if pickler in backend_module.unpatch:
-        logger.info("backend: %r: unpatch", backend)
+        logger.debug("backend: %r, %r will be set to the default.", backend, pickler)
+        logger.debug("backend: %r: unpatch", backend)
         backend_module.monkey_unpatch()
         return
     pickler_module = _import_pickler(pickler)
-    logger.info("backend: %r, pickler: %r: patch", backend, pickler)
+    logger.debug("backend: %r, pickler: %r: patch", backend, pickler)
     backend_module.monkey_patch(pickler, pickler_module.Pickler)
 
 
@@ -36,10 +37,23 @@ def _import_pickler(pickler: PicklerType) -> PicklerModule:
 
 
 def _validate_pickler(
+    backend_name: BackendType,
     backend: BackendModule,
     pickler: PicklerType | None,
 ) -> PicklerType:
-    pickler = pickler or backend.order[0]
+    if not pickler:
+        logger.debug(
+            "backend: %r, pickler is not specified. use default: %r.",
+            backend_name,
+            backend.order[0],
+        )
+        pickler = backend.order[0]
     if pickler in backend.replace:
+        logger.debug(
+            "backend: %r, %r is replaced by %r.",
+            backend_name,
+            pickler,
+            backend.replace[pickler],
+        )
         pickler = backend.replace[pickler]
     return pickler
