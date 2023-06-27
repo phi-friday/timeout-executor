0b4f50f399856babadc2006b3a58c48caaa378e9
phi <phi.friday@gmail.com>
2023-06-27 22:39:23 +0900

    fix: loky monkey_patch with lock

--- src/timeout_executor/pickler/_loky/patch.py
+++ src/timeout_executor/pickler/_loky/patch.py
@@ -17,34 +17,40 @@ order: tuple[PicklerType, ...] = ("cloudpickle", "dill")
 
 def monkey_patch(name: str, pickler: Pickler) -> None:  # noqa: ARG001
     """patch loky"""
-    if name == "pickle":
-        logger.warning("loky uses cloudpickle as the default")
-        name = "cloudpickle"
-    try:
-        from loky.backend.reduction import (  # type: ignore
-            get_loky_pickler_name,  # type: ignore
-            set_loky_pickler,  # type: ignore
-        )
-    except ImportError as exc:
-        error = ExtraError.from_import_error(exc, extra="loky")
-        raise error from exc
+    from timeout_executor.pickler.lock import patch_lock
 
-    if get_loky_pickler_name() == name:
-        return
-    set_loky_pickler(name)
+    with patch_lock:
+        if name == "pickle":
+            logger.warning("loky uses cloudpickle as the default")
+            name = "cloudpickle"
+        try:
+            from loky.backend.reduction import (  # type: ignore
+                get_loky_pickler_name,  # type: ignore
+                set_loky_pickler,  # type: ignore
+            )
+        except ImportError as exc:
+            error = ExtraError.from_import_error(exc, extra="loky")
+            raise error from exc
+
+        if get_loky_pickler_name() == name:
+            return
+        set_loky_pickler(name)
 
 
 def monkey_unpatch() -> None:
     """unpatch loky"""
-    try:
-        from loky.backend.reduction import (  # type: ignore
-            get_loky_pickler_name,  # type: ignore
-            set_loky_pickler,  # type: ignore
-        )
-    except ImportError as exc:
-        error = ExtraError.from_import_error(exc, extra="loky")
-        raise error from exc
+    from timeout_executor.pickler.lock import patch_lock
 
-    if get_loky_pickler_name == "cloudpickle":
-        return
-    set_loky_pickler()
+    with patch_lock:
+        try:
+            from loky.backend.reduction import (  # type: ignore
+                get_loky_pickler_name,  # type: ignore
+                set_loky_pickler,  # type: ignore
+            )
+        except ImportError as exc:
+            error = ExtraError.from_import_error(exc, extra="loky")
+            raise error from exc
+
+        if get_loky_pickler_name == "cloudpickle":
+            return
+        set_loky_pickler()
