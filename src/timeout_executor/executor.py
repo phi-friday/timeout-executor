a0ebdaf5acd281cba8d6e564530794944695f06c
phi <phi.friday@gmail.com>
2023-06-28 20:27:10 +0900

    feat: external executor

--- src/timeout_executor/executor.py
+++ src/timeout_executor/executor.py
@@ -2,13 +2,24 @@ from __future__ import annotations
 
 import asyncio
 from concurrent.futures import wait
+from contextlib import contextmanager
 from functools import partial
-from typing import TYPE_CHECKING, Any, Callable, Coroutine, Literal, TypeVar, overload
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Coroutine,
+    Generator,
+    Literal,
+    TypeVar,
+    overload,
+)
 
 import anyio
 from typing_extensions import ParamSpec
 
 from timeout_executor.concurrent import get_executor_backend
+from timeout_executor.log import logger
 from timeout_executor.serde import monkey_patch
 from timeout_executor.serde.lock import patch_lock
 
@@ -39,18 +50,83 @@ class TimeoutExecutor:
         timeout: float,
         backend: BackendType | None = None,
         pickler: PicklerType | None = None,
+        *,
+        executor: billiard_future.ProcessPoolExecutor
+        | multiprocessing_future.ProcessPoolExecutor
+        | loky_future.ProcessPoolExecutor
+        | None = None,
     ) -> None:
         self.timeout = timeout
         self._init = None
         self._args = ()
         self._kwargs = {}
         self._select = (backend, pickler)
+        self._executor = executor
+        self._external_executor = executor is not None
 
     @property
     def lock(self) -> RLock:
         """patch lock"""
 
         return patch_lock
+
+    @property
+    def executor(
+        self,
+    ) -> (
+        billiard_future.ProcessPoolExecutor
+        | multiprocessing_future.ProcessPoolExecutor
+        | loky_future.ProcessPoolExecutor
+    ):
+        """process pool executor"""
+        if self._executor is None:
+            self._executor = get_executor(self._select[0], self._select[1])(
+                1,
+                initializer=self._partial_init(),
+            )
+        return self._executor
+
+    @executor.setter
+    def executor(
+        self,
+        executor: billiard_future.ProcessPoolExecutor
+        | multiprocessing_future.ProcessPoolExecutor
+        | loky_future.ProcessPoolExecutor,
+    ) -> None:
+        if self._executor is not None:
+            raise AttributeError("executor already exists")
+        self._executor = executor
+        self._external_executor = True
+
+    @executor.deleter
+    def executor(self) -> None:
+        if self._executor is None:
+            return
+        if self._executor._executor_manager_thread_wakeup is None:  # noqa: SLF001
+            self._executor = None
+            return
+        self._executor = None
+        self._external_executor = False
+
+    @contextmanager
+    def _enter(
+        self,
+    ) -> Generator[
+        billiard_future.ProcessPoolExecutor
+        | multiprocessing_future.ProcessPoolExecutor
+        | loky_future.ProcessPoolExecutor,
+        None,
+        None,
+    ]:
+        executor = self.executor
+        try:
+            yield executor
+        finally:
+            if self._external_executor:
+                logger.warning("shutdown executor yourself")
+            else:
+                executor.shutdown(False, True)  # noqa: FBT003
+            del self.executor
 
     def _partial_init(self) -> Callable[[], Any] | None:
         if self._init is None:
@@ -92,8 +168,7 @@ class TimeoutExecutor:
         Returns:
             pickable func result
         """
-        executor = get_executor(self._select[0], self._select[1])
-        with executor(1, initializer=self._partial_init()) as pool:
+        with self._enter() as pool:
             future = pool.submit(func, *args, **kwargs)
             wait([future], timeout=self.timeout)
             if not future.done():
@@ -122,8 +197,7 @@ class TimeoutExecutor:
         Returns:
             pickable func result
         """
-        executor = get_executor(self._select[0], self._select[1])
-        with executor(1, initializer=self._partial_init()) as pool:
+        with self._enter() as pool:
             try:
                 future = pool.submit(
                     _async_run,
